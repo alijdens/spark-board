a35be8e8aa0724c67e303771b155a3f0a5566fa6
Axel Lijdens <axel.lijdens@gmail.com>
2023-03-29 23:29:54 -0300

    Add example to test easily the parser.

---
+++ tests/examples/select_and_filter.py
@@ -0,0 +1,19 @@
+# from context import spark
+from plan_extractor.explain_generator import ExplainGenerator
+from plan_extractor.explain_parser import ExplainParser
+
+
+# df = spark.createDataFrame([], schema="struct<dni:int, name:string, age:int, weight:float, city:string>")
+# df = df.filter(df.age > 18)
+# df = df.filter(df.city == "CABA")
+# df = df.select("name", "age", df.dni.alias("user DNI"))
+
+# plan = ExplainGenerator(df=df).generate()
+plan = """
+'Project [*]
++- Project [name#1, age#2, dni#0 AS user DNI#10]
+   +- Filter (CABA = city#4)
+"""
+print(plan)
+
+ExplainParser().parse_graph(plan=plan)
