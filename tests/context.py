d01e64f6d158443224546e7e9f92a5818fcfeb4b
Axel Lijdens <axel.lijdens@gmail.com>
2023-04-18 23:55:38 -0300

    Update Spark context builder to locate the warehouse in a temporary directory.

--- tests/context.py
+++ tests/context.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import os
+import sys
+import tempfile
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType
-from pyspark.sql import functions as F
-from pyspark.sql.window import Window
 
-sc = SparkContext('local')
-spark = SparkSession(sc)
-spark.conf.set("spark.sql.debug.maxToStringFields", 1000)
+
+temp_dir = tempfile.TemporaryDirectory().name
+spark_warehouse_dir = os.path.join(temp_dir, "spark-warehouse")
+metastore_db_dir = os.path.join(temp_dir, "metastore_db")
+connection_url = f"jdbc:derby:;databaseName={metastore_db_dir};create=true"
+
+spark = (
+    SparkSession.builder.master("local")
+    .config("spark.sql.debug.maxToStringFields", 1000)
+    .config("spark.sql.warehouse.dir", spark_warehouse_dir)
+    .config("javax.jdo.option.ConnectionURL", connection_url)
+    .getOrCreate()
+)
