fe65486629573bf9e9119192d216be9fab931e1f
Axel Lijdens <axel.lijdens@gmail.com>
2023-06-09 00:03:32 -0300

    Remove unused import.

--- spark-board-ui/src/main.jsx
+++ spark-board-ui/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ReactFlow, { ReactFlowProvider } from 'reactflow';
+import { ReactFlowProvider } from 'reactflow';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
