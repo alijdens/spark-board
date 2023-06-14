47ac6b130ef16458ee38fa5c89d33180920c27d0
Ezequiel Werner <eze210@gmail.com>
2023-06-13 22:41:22 -0300

    Adding missing css baseline

--- spark-board-ui/src/main.jsx
+++ spark-board-ui/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { ReactFlowProvider } from 'reactflow';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 // We do this to force light theme regardless of the device's default theme.
 const lightTheme = createTheme({
@@ -15,6 +16,7 @@ const lightTheme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
       <ReactFlowProvider>
         <App />
       </ReactFlowProvider>
