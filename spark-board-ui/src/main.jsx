7c504c66e9070ab0d16dbacf83aa0f18d6099186
Ezequiel Werner <eze210@gmail.com>
2023-06-15 18:58:48 -0300

    Force light theme (fixes #24) (#25)

--- spark-board-ui/src/main.jsx
+++ spark-board-ui/src/main.jsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ReactFlowProvider } from 'reactflow';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// We do this to force light theme regardless of the device's default theme.
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ReactFlowProvider>
-      <App />
-    </ReactFlowProvider>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
