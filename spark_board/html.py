893c37c7bb4e3dfd7b5cb73556df21e0d5f18f39
alijdens <63980668+alijdens@users.noreply.github.com>
2023-06-25 22:24:58 -0300

    Add descriptions to the default variables.

--- spark_board/html.py
+++ spark_board/html.py
@@ -28,9 +28,13 @@ class Encoder(json.JSONEncoder):
 
 @dataclasses.dataclass
 class DefaultSettings:
-    """Default settings for the HTML DAG renderer."""
+    """Default settings for the HTML DAG renderer. The settings can be changed through the UI, but
+    here we only define the default value they'll take when the website loads."""
 
+    # enable or disable all "bouncy" node animations
     animationEnabled: bool = True
+
+    # nodes are animated (pull linked nodes) when dragged with the mouse on the screen
     animationEnabledOnDrag: bool = True
 
 
