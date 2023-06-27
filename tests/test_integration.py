a779d760ad6fa19c9957ef3d814187896ef336c9
Axel Lijdens <axel.lijdens@gmail.com>
2023-06-26 23:13:06 -0300

    Make the temporal files in a test be deleted when it finishes.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,10 +1,9 @@
-import pytest
 from pyspark.sql.session import SparkSession
 from spark_board.html import dump_dataframe, DefaultSettings
 from pathlib import Path
 
 
-def test_spark_board(spark: SparkSession) -> None:
+def test_spark_board(spark: SparkSession, tmp_path: Path) -> None:
     """This test intends to be a very simple smoke test for the normal spark-board
     usage."""
 
@@ -12,7 +11,7 @@ def test_spark_board(spark: SparkSession) -> None:
     df = spark.createDataFrame([], schema="struct<a:double, b:double, c:double>")
     df = df.withColumn("sum", (df.a + df.b) / df.c)
 
-    OUT_DIR = Path("spark_board_output")
+    OUT_DIR = tmp_path/"spark_board_output"
 
     # dump it expecting no errors
     dump_dataframe(
