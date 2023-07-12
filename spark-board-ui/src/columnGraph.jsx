62622a3af9a8792eb8cf5b5e6063d57959e72a46
Ezequiel Werner <eze210@gmail.com>
2023-07-12 19:47:32 -0300

    recursion -> iteration

--- spark-board-ui/src/columnGraph.jsx
+++ spark-board-ui/src/columnGraph.jsx
@@ -97,12 +97,25 @@ export default drawColumnGraph;
  * @returns 
  */
 function buildColumnGraph(nodesById, column) {
-    if (column == null) {
+    if (column === null || column === undefined) {
         return [];
     }
-    return [column.id].concat(column.data.linked_columns.map((linkedId) =>
-        buildColumnGraph(nodesById, nodesById.get(linkedId))
-    )).flat();
+    let columnIds = [column.id];
+    let links = Array.from(column.data.linked_columns);
+    while (links.length > 0) {
+        let currentColumnId = links.pop();
+        let node = nodesById.get(currentColumnId);
+        if (node == null) {
+            console.log("Node not found for id: " + currentColumnId);
+            continue;
+        }
+        links.concat(node.data.linked_columns);
+        columnIds.push(currentColumnId);
+
+        console.log("Partial: " + columnIds);
+    }
+    console.log("Final: " + columnIds);
+    return columnIds;
 }
 
 function applyColumnNodeEffectOnColumnTrackingChanged(currentNode, columnTracking, visitedTransformationNodes) {
