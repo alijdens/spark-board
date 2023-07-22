bc7c851d971ffe6a6cd7045d6e689e35cb982b98
Ezequiel Werner <eze210@gmail.com>
2023-07-21 22:25:58 -0300

    Add doc

--- spark_board/plan_extractor/dag_simplification.py
+++ spark_board/plan_extractor/dag_simplification.py
@@ -4,11 +4,19 @@ from typing import Dict, List, Set, TypeVar
 
 
 class DagSimplifier(object):
+    """
+    The DAG Simplifiers detect certain pattern in a DAG, and map them to
+    a simplified version of it.
+    For example, four filters in a row could be simplified as one filter
+    with four conditions.
+    """
 
     def can_apply(self, dag: TransformationNode) -> bool:
+        """Determines whether a pattern can be simplified by this simplifier or not"""
         raise NotImplementedError("Abstract method")
     
     def apply(self, original: TransformationNode) -> TransformationNode:
+        """Returns a simplified version of the original DAG"""
         raise NotImplementedError("Abstract method")
 
 
