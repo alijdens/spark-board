f529036dc646b6bd18fde19d56e5a7d15edc02f6
Ezequiel Werner <eze210@gmail.com>
2023-09-03 16:46:11 -0300

    missing builders

---
+++ spark_board/plan_extractor/transformation_node_builders/streaming_relation_node_builder.py
@@ -0,0 +1,17 @@
+from py4j.java_gateway import JavaObject
+from typing import Optional
+
+from ..transformations_dag import Metadata, TransformationType
+from .transformation_node_builder import TransformationNodeBuilder
+
+
+class StreamingRelationNodeBuilder(TransformationNodeBuilder):
+    def _extract_metadata(self, node: JavaObject, metadata: Metadata) -> None:
+        metadata["type"] = "relation"
+        metadata["source_name"] = node.toString()
+
+    def _get_type(self) -> TransformationType:
+        return TransformationType.StreamingRelation
+
+    def _expected_number_of_nodes(self) -> Optional[int]:
+        return 0
